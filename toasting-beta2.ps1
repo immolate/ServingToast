8b6c996bc394759c6ba62aaaf59afaac462fbec7
David Hamilton <david.hamilton@smartdolphins.com>
2022-01-01 14:26:14 -0800

    Whatever

--- toasting-beta2.ps1
+++ toasting-beta2.ps1
@@ -1,27 +1,83 @@
-﻿$scriptName = $script:MyInvocation.MyCommand.Name
-    $scriptPath = [IO.Path]::GetDirectoryName($script:MyInvocation.MyCommand.Path)
-    $scriptFullName = (Join-Path $scriptPath $scriptName)
+﻿<#
+.SYNOPSIS
+  
+  This script is intended to display toasts to the logged in user. 
+.DESCRIPTION
+  
+  
 
-    if ((Get-AuthenticodeSignature -FilePath $scriptFullName).Status -eq "NotSigned") {
-        Write-Warning "Script is not signed - skipping"
-        return $false
-    }
+.PARAMETER <Parameter_Name>
+  
+    CompanyName - Your company name
+    LogoImage - <required> base 64 representation or URL to your publicly available logo
+    HeroImage - <optional> base 64 representation of URL to your publicly available other image
+    Title/other - TBD
 
-    $oldName = [IO.Path]::GetFileNameWithoutExtension($scriptName) + ".old"
-    $oldFullName = (Join-Path $scriptPath $oldName)
+.INPUTS
 
-    $tempFullName = (Join-Path $env:TEMP $scriptName)
+  None
 
-    $ScriptVersion = "1.01"
-    $ScriptUrl = "https://github.com/immolate/ServingToast/ScriptVersions.csv"
+.OUTPUTS
 
-$Load = [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]
-$Load = [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]
+  A toast. Cheers. 
+
+.NOTES
+  Version:        1.01
+  Author:         David Hamilton
+  Creation Date:  December 31, 2021
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+
+  ./toasting-beta2.ps1 -whatever whatever -whatever2 whatever2
+  
+.Changelog
+
+2021-12-31 - Research - :
+    Version 1.00
+    ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    https://docs.microsoft.com/en-us/windows/apps/design/shell/tiles-and-notifications/toast-progress-bar?tabs=builder-syntax#using-data-binding-to-update-a-toast
+    https://docs.microsoft.com/en-us/uwp/api/windows.ui.notifications.toasttemplatetype?view=winrt-22000
+    https://docs.microsoft.com/et-ee/uwp/api/windows.ui.notifications?view=winrt-19041
+    Check existing scripts. Some cool ideas - but WAY too extensive/overboard with code. I plan to make this simple and effective. While it's working already at less than 
+    100 lines of code - I plan to add at least a few hundred other lines for advanced functionality. 
+    Borrowed some basic MS code to automatically update script version from github if it's not compliant. 
+
+2022-01-01
+
+    Version 1.01
+    ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    Generalize code
+    Add version file
+    Add licensing
+
+
+
+#>
+$scriptName = $script:MyInvocation.MyCommand.Name
+$scriptPath = [IO.Path]::GetDirectoryName($script:MyInvocation.MyCommand.Path)
+$scriptFullName = (Join-Path $scriptPath $scriptName)
+
+if ((Get-AuthenticodeSignature -FilePath $scriptFullName).Status -eq "NotSigned") {
+    Write-Warning "Script is not signed - skipping"
+    return $false
+}
+
+$oldName = [IO.Path]::GetFileNameWithoutExtension($scriptName) + ".old"
+$oldFullName = (Join-Path $scriptPath $oldName)
+$tempFullName = (Join-Path $env:TEMP $scriptName)
+$ScriptVersion = "1.01"
+$ScriptUrl = "https://github.com/immolate/ServingToast/ScriptVersions.csv"
+
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]
+[Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]
             
-#[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
-#[Windows.UI.Notifications.ToastNotification, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
-#[Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
 $app_ID = 'ae02226e-fb1e-40e4-bb01-17ff41a39ad5'
+<#
+TODO
+LogoImage, HeroImage, CompanyName, Title/other messages generalized to be Params instead of set directly
+
+#>
 $LogoImage = ""
 $HeroImage = ""
 $CompanyName = ""
@@ -29,6 +85,7 @@ $CompanyName = ""
 <#
 binding templates: ToastImageAndText01-4
 ToastText01-04
+Desire ToastGeneric - not working yet
 #>
 [xml]$template = @"
 <toast scenario="reminder">
